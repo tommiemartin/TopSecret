c0d693a516d3e118f56c4594fc53f1c27c3a0624
tommiemartin <tomklawender@gmail.com>
2022-04-17 00:47:34 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+r = ""
+
+y = "Mn ymjwj1 N stynhji ~tzw uwtknqj ts Gzrgqj fsi |fx nsywnlzji3 N,r f gny tk f ij{jqtujw r~xjqk3 \
+Nk ~tz |fsy fii rj tshj ~tz knlzwj tzy ymnx mn2yjhm jsh~wuynts ?U \
+Ytrrnj"
+
+for i in y:
+    if i == " ":
+        r += " "
+    else:
+        r += chr(ord(i) -5)
+
+print (r)
